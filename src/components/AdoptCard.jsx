04a6cb900bca39ae71441e0b415ee04fa74e8be8
pekr0002 <pekr0002@stud.kea.dk>
2025-11-10 13:34:40 +0100

    add dynamic petlist

--- src/components/AdoptCard.jsx
+++ src/components/AdoptCard.jsx
@@ -2,45 +2,46 @@ import Link from "next/link";
 import Image from "next/image";
 import { FaRegStar } from "react-icons/fa";
 
-const AdoptCard = () => {
+const AdoptCard = ({ img }) => {
   return (
-    <article className="h-fit w-40 rounded-2xl bg-white shadow-[0_4px_16px_0_rgba(19,21,68,0.06)]">
-      {/* link */}
-      <Link href="/details">
-        <div className="grid grid-cols-1 grid-rows-1">
-          {/* img */}
-          <Image
-            src="https://cataas.com/cat"
-            alt="Adoptable cat"
-            width={160}
-            height={114}
-            loading="eager"
-            className="col-[1/2] row-[1/2] h-[114px] w-40 rounded-2xl object-cover"
-          />
-          <button
-            onClick={console.log("fav")}
-            className="col-[1/2] row-[1/2] m-2 self-start justify-self-end rounded-full bg-[#FEFEFE]/40 p-1"
-          >
-            <FaRegStar className="self-center justify-self-center" />
-          </button>
-        </div>
-        {/* information */}
-        <div className="grid grid-cols-2 px-2.5 pt-2 pb-4">
-          {/* name */}
-          <h2 className="text-[15px] font-bold text-[#333333] capitalize">
-            harry
-          </h2>
-          {/* age */}
-          <h3 className="self-end justify-self-end text-[12px] text-[#ADADAD] capitalize">
-            Young
-          </h3>
-          {/* race */}
-          <h3 className="h-7 w-20 justify-start font-['Manrope'] text-xs leading-4 font-medium text-zinc-800 capitalize opacity-40">
-            Yorkshire Terrier
-          </h3>
-        </div>
-      </Link>
-    </article>
+    // <article className="h-fit w-40 rounded-2xl bg-white shadow-[0_4px_16px_0_rgba(19,21,68,0.06)]">
+    //   {/* link */}
+    //   <Link href="/details">
+    //     <div className="grid grid-cols-1 grid-rows-1">
+    //       {/* img */}
+    //       <Image
+    //         src={img}
+    //         alt="Adoptable cat"
+    //         width={160}
+    //         height={114}
+    //         loading="eager"
+    //         className="col-[1/2] row-[1/2] h-[114px] w-40 rounded-2xl object-cover"
+    //       />
+    //       <button
+    //         onClick={console.log("fav")}
+    //         className="col-[1/2] row-[1/2] m-2 self-start justify-self-end rounded-full bg-[#FEFEFE]/40 p-1"
+    //       >
+    //         <FaRegStar className="self-center justify-self-center" />
+    //       </button>
+    //     </div>
+    //     {/* information */}
+    //     <div className="grid grid-cols-2 px-2.5 pt-2 pb-4">
+    //       {/* name */}
+    //       <h2 className="text-[15px] font-bold text-[#333333] capitalize">
+    //         harry
+    //       </h2>
+    //       {/* age */}
+    //       <h3 className="self-end justify-self-end text-[12px] text-[#ADADAD] capitalize">
+    //         Young
+    //       </h3>
+    //       {/* race */}
+    //       <h3 className="h-7 w-20 justify-start font-['Manrope'] text-xs leading-4 font-medium text-zinc-800 capitalize opacity-40">
+    //         Yorkshire Terrier
+    //       </h3>
+    //     </div>
+    //   </Link>
+    // </article>
+    <p>bruges ikke</p>
   );
 };
 
--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -1,19 +1,53 @@
-import AdoptCard from "./AdoptCard";
+// import AdoptCard from "./AdoptCard";
+import Link from "next/link";
+import Image from "next/image";
+import { FaRegStar } from "react-icons/fa";
+import { Suspense } from "react";
+
 const PetList = () => {
   return (
-    <section className="grid grid-cols-2 gap-4 py-4">
-      <AdoptCard />
-      <AdoptCard />
-
-      <AdoptCard />
-
-      <AdoptCard />
-      <AdoptCard />
-      <AdoptCard />
-      <AdoptCard />
-      <AdoptCard />
-    </section>
+    <Suspense>
+      <FetchProducts />
+    </Suspense>
   );
+};
+
+const FetchProducts = async () => {
+  const response = await fetch("https://dummyjson.com/products");
+  const { products } = await response.json();
+  return products.map((product) => (
+    <Link
+      href="/details"
+      className="h-fit w-40 rounded-2xl bg-white shadow-[0_4px_16px_0_rgba(19,21,68,0.06)]"
+      key={product.id}
+    >
+      <div className="grid grid-cols-1 grid-rows-1">
+        <Image
+          src={product.images[0]}
+          alt="Adoptable cat"
+          width={160}
+          height={114}
+          loading="eager"
+          className="col-[1/2] row-[1/2] h-[114px] w-40 rounded-2xl object-cover"
+        />
+        <button className="col-[1/2] row-[1/2] m-2 self-start justify-self-end rounded-full bg-[#FEFEFE]/40 p-1">
+          <FaRegStar className="self-center justify-self-center" />
+        </button>
+      </div>
+      <div className="grid grid-cols-2 px-2.5 pt-2 pb-4">
+        <h2 className="text-[15px] font-bold text-[#333333] capitalize">
+          {product.title}
+        </h2>
+        <h3 className="self-end justify-self-end text-[12px] text-[#ADADAD] capitalize">
+          {product.price},-
+        </h3>
+        {/* race */}
+        <h3 className="h-7 w-20 justify-start font-['Manrope'] text-xs leading-4 font-medium text-zinc-800 capitalize opacity-40">
+          {product.category}
+        </h3>
+      </div>
+    </Link>
+  ));
 };
 
 export default PetList;
