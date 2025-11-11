cfa950d325a957365850d733e0d3223a5071fd48
pekr0002 <pekr0002@stud.kea.dk>
2025-11-11 12:52:12 +0100

    random background

--- src/app/details/[id]/page.js
+++ src/app/details/[id]/page.js
@@ -2,21 +2,18 @@ import Image from "next/image";
 import { FaRegStar } from "react-icons/fa";
 import { IoIosArrowBack } from "react-icons/io";
 import Link from "next/link";
+import { Suspense } from "react";
 
 import InfoCard from "@/components/InfoCard";
 
 const details = async ({ params }) => {
   const { id } = await params;
-  // Fetch the product using the dynamic ID from the URL
   const response = await fetch(`https://dummyjson.com/products/${id}`);
   const product = await response.json();
-  console.log(product);
 
   return (
-    <>
-      {/* hero */}
-      <div className="grid grid-cols-1 grid-rows-1">
-        {/* img */}
+    <Suspense>
+      <div className="grid grid-cols-1 grid-rows-1 rounded-4xl bg-[#EABC4E]">
         <Image
           src={product.images[0]}
           alt={product.title}
@@ -48,13 +45,12 @@ const details = async ({ params }) => {
             loading="eager"
             className="col-[1/2] row-[1/3] h-8 w-8 rounded-lg object-cover"
           />
-          <p className="w-20 justify-start text-center font-['Manrope'] text-xs leading-4 font-medium text-white/90">
+          <p className="w-20 justify-start text-center text-xs leading-4 font-medium text-white/90">
             Baton Rouge
           </p>
         </div>
       </div>
-      {/* information */}
-      <h1 className="my-3 justify-start font-['Manrope'] text-3xl leading-8 font-bold text-zinc-800">
+      <h1 className="my-3 justify-start text-3xl leading-8 font-bold text-zinc-800">
         {product.title}
       </h1>
       <div className="my-4 flex gap-2">
@@ -62,39 +58,22 @@ const details = async ({ params }) => {
           <InfoCard key={index} tag={tag} />
         ))}
       </div>
-      <div className="h-24 w-80 justify-start font-['Manrope'] text-base leading-6 font-medium text-zinc-800 opacity-75">
-        Den venligste Samojed, vi nogensinde har mødt. Elsker at lege med bolde
-        og er venlig over for andre dyr. På trods af sin hvide pels elsker den
-        regn og vandpytter. <br />
+      <div className="h-24 w-80 justify-start text-base leading-6 font-medium text-zinc-800 opacity-75">
+        {product.description}
       </div>
-      <div className="justify-start font-['Manrope'] text-xs leading-5 font-medium text-zinc-800 opacity-50">
-        Opdateret den 13. juli.
+      <div className="my-6 justify-start text-xs leading-5 font-medium text-zinc-800 opacity-50">
+        Rating: {product.rating}★
       </div>
       <div className="my-2 inline-flex w-full items-center justify-center gap-2.5 rounded-[60px] bg-red-300 px-2.5 py-4">
         <div className="justify-start">
-          <span className="font-['Manrope'] text-base leading-6 font-bold text-white">
-            Adopter
-          </span>
-          <span className="font-['Manrope'] text-base leading-6 font-medium text-white">
-            {" "}
-            Samojed Willie
+          <span className="text-base leading-6 font-bold text-white">BUY</span>
+          <span className="mx-[5px] text-base leading-6 font-medium text-white">
+            {product.title}
           </span>
         </div>
       </div>
-    </>
+    </Suspense>
   );
 };
-
-// const FetchTags = async () => {
-//   const response = await fetch("https://dummyjson.com/products");
-//   const { products } = await response.json();
-//   return products.map((product) => (
-//     <div className="inline-flex items-center justify-center gap-2.5 rounded-[20px] bg-lime-200 px-3.5 py-2">
-//       <div className="justify-start font-['Manrope'] text-xs leading-4 font-medium text-stone-500">
-//         Young
-//       </div>
-//     </div>
-//   ));
-// };
 
 export default details;
--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -13,41 +13,55 @@ const PetList = () => {
 };
 
 const FetchProducts = async () => {
-  const response = await fetch("https://dummyjson.com/products");
+  const colors = [
+    "#f5d0c7",
+    "#ec8b84",
+    "#efa9b3",
+    "#ebbd59",
+    "#bbedec",
+    "#a2cf8a",
+  ];
+  const response = await fetch("https://dummyjson.com/products/");
   const { products } = await response.json();
-  return products.map((product) => (
-    <Link
-      href={`/details/${product.id}`}
-      className="h-fit w-40 rounded-2xl bg-white shadow-[0_4px_16px_0_rgba(19,21,68,0.06)]"
-      key={product.id}
-    >
-      <div className="grid grid-cols-1 grid-rows-1">
-        <Image
-          src={product.images[0]}
-          alt={product.title}
-          width={160}
-          height={114}
-          loading="eager"
-          className="col-[1/2] row-[1/2] h-[114px] w-40 rounded-2xl object-cover"
-        />
-        <button className="col-[1/2] row-[1/2] m-2 self-start justify-self-end rounded-full bg-[#FEFEFE]/40 p-1">
-          <FaRegStar className="self-center justify-self-center" />
-        </button>
-      </div>
-      <div className="grid grid-cols-2 px-2.5 pt-2 pb-4">
-        <h2 className="text-[15px] font-bold text-[#333333] capitalize">
-          {product.title}
-        </h2>
-        <h3 className="self-end justify-self-end text-[12px] text-[#ADADAD] capitalize">
-          {product.price},-
-        </h3>
-        {/* race */}
-        <h3 className="h-7 w-20 justify-start font-['Manrope'] text-xs leading-4 font-medium text-zinc-800 capitalize opacity-40">
-          {product.category}
-        </h3>
-      </div>
-    </Link>
-  ));
+  return products.map((product) => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    return (
+      <Link
+        href={`/details/${product.id}`}
+        className="h-fit w-40 rounded-2xl bg-white shadow-[0_4px_16px_0_rgba(19,21,68,0.06)]"
+        key={product.id}
+      >
+        <div
+          className="grid grid-cols-1 grid-rows-1 rounded-2xl"
+          style={{ backgroundColor: randomColor }}
+        >
+          <Image
+            src={product.images[0]}
+            alt={product.title}
+            width={160}
+            height={114}
+            loading="eager"
+            className="col-[1/2] row-[1/2] h-[114px] w-40 rounded-2xl object-cover"
+          />
+          <button className="col-[1/2] row-[1/2] m-2 self-start justify-self-end rounded-full bg-[#FEFEFE]/40 p-1">
+            <FaRegStar className="self-center justify-self-center" />
+          </button>
+        </div>
+        <div className="grid grid-cols-2 px-2.5 pt-2 pb-4">
+          <h2 className="text-[15px] font-bold text-[#333333] capitalize">
+            {product.title}
+          </h2>
+          <h3 className="self-end justify-self-end text-[12px] text-[#ADADAD] capitalize">
+            {product.price},-
+          </h3>
+
+          <h3 className="h-7 w-20 justify-start font-['Manrope'] text-xs leading-4 font-medium text-zinc-800 capitalize opacity-40">
+            {product.brand ? product.brand : "Unknown Brand"}
+          </h3>
+        </div>
+      </Link>
+    );
+  });
 };
 
 export default PetList;
