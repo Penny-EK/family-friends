a533003a74e456304d1ff2f78365bd60c71ed545
pekr0002 <pekr0002@stud.kea.dk>
2025-11-11 12:44:02 +0100

    fetch categories

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,14 @@
 import Image from "next/image";
 // components
 import PetList from "@/components/PetList";
-import FilterOption from "@/components/FilterOption";
+import FilterOptions from "@/components/FilterOptions";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="bg-white">
       <section className="flex gap-4 overflow-x-auto py-4">
-        <FilterOption />
-        <FilterOption />
-        <FilterOption />
-        <FilterOption />
-        <FilterOption />
+        <FilterOptions />
       </section>
       <section className="grid grid-cols-2 gap-4 py-4">
         <PetList />
